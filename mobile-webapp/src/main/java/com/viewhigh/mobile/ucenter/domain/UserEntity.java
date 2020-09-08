6a554439a0cacbae56b77430c9bd64dc01feb07b
chentaoyu <chenty75@gmail.com>
2020-09-08 11:15:18 +0800

    调整 pom 依赖，新增User 实体类

---
+++ mobile-webapp/src/main/java/com/viewhigh/mobile/ucenter/domain/UserEntity.java
@@ -0,0 +1,131 @@
+package com.viewhigh.mobile.ucenter.domain;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "ucenter_user")
+public class UserEntity {
+    private String userId;
+    private String username;
+    private String password;
+    private String salt;
+    private String nickname;
+    private Byte sex;
+    private String avatar;
+    private Timestamp createTime;
+    private String createIp;
+    private Timestamp lastLoginTime;
+    private String lastLoginIp;
+
+    @Id
+    @Column(name = "user_id", nullable = false, length = 32)
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    @Basic
+    @Column(name = "username", nullable = false, length = 100)
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Basic
+    @Column(name = "password", nullable = true, length = 100)
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Basic
+    @Column(name = "salt", nullable = true, length = 32)
+    public String getSalt() {
+        return salt;
+    }
+
+    public void setSalt(String salt) {
+        this.salt = salt;
+    }
+
+    @Basic
+    @Column(name = "nickname", nullable = true, length = 20)
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    @Basic
+    @Column(name = "sex", nullable = true)
+    public Byte getSex() {
+        return sex;
+    }
+
+    public void setSex(Byte sex) {
+        this.sex = sex;
+    }
+
+    @Basic
+    @Column(name = "avatar", nullable = true, length = 100)
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    @Basic
+    @Column(name = "create_time", nullable = false)
+    public Timestamp getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Timestamp createTime) {
+        this.createTime = createTime;
+    }
+
+    @Basic
+    @Column(name = "create_ip", nullable = true, length = 50)
+    public String getCreateIp() {
+        return createIp;
+    }
+
+    public void setCreateIp(String createIp) {
+        this.createIp = createIp;
+    }
+
+    @Basic
+    @Column(name = "last_login_time", nullable = true)
+    public Timestamp getLastLoginTime() {
+        return lastLoginTime;
+    }
+
+    public void setLastLoginTime(Timestamp lastLoginTime) {
+        this.lastLoginTime = lastLoginTime;
+    }
+
+    @Basic
+    @Column(name = "last_login_ip", nullable = true, length = 50)
+    public String getLastLoginIp() {
+        return lastLoginIp;
+    }
+
+    public void setLastLoginIp(String lastLoginIp) {
+        this.lastLoginIp = lastLoginIp;
+    }
+
+   }
